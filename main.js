aa430855d8c32050eb6e89ca4a6a6c21eee1c172
steve <stevetak@outlook.fr>
2024-10-24 16:13:30 +0200

    creation page chat

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 
+// Sélectionne l'image 
+const image = document.getElementById('myImage');
 
+
+// Ajoute un écouteur d'événement pour détecter le clic sur l'image
+image.addEventListener('click', function() {
+  alert('Chaton mignon');
+});
